ab7b2d10a8ef8aa64d44a353b952eef601855e75
HrishikeshPujari <hrishikesh.pujari@resettech.in>
2024-02-13 16:48:51 +0530

    - hashing password - user schema - Bad request Erorr

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import 'express-async-errors'; // to handle throw errors in async function without next();
+import mongoose from 'mongoose';
 import { json } from 'body-parser';
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -20,7 +21,17 @@ app.all('*', () => {
 });
 app.use(errorHandler);
 
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+        console.log("Connected to MongoDB");
+    } catch (err) {
+        console.error(err);
+    }
+    app.listen(3000, () => {
+        console.log('Listening on port 3000!!!');
+    });
+}
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000!!!');
-});
+start();
+
